3239e843d55013611dd9d6d44045b957b7bbfbce
Mayer Olivares Salinas <mayerolivares_15@hotmail.com>
2016-09-01 01:20:24 -0500

    Creando la clase que nos va a permitir acceder a los datos almacenados.

--- AplicacionVentaAutomoviles/src/VentaAutomoviles/c4_Persistencia/daoPostgreSql/GestorJDBCSqlServer.java
+++ AplicacionVentaAutomoviles/src/VentaAutomoviles/c4_Persistencia/daoPostgreSql/GestorJDBCSqlServer.java
@@ -12,7 +12,9 @@ public class GestorJDBCSqlServer extends GestorJDBC{
 
     @Override
     public void abrirConexion() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Class.forName("org.sqlserver.Driver");
+        String url = "jdbc:sqlserver://localhost:5432/VentaAutomoviles";
+        
     }
     
 }
